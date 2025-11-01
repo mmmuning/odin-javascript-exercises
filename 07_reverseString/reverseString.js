cd0bbc24179d347ef0df8d7ff19696877f924cf1
mmmuning <217378167+mmmuning@users.noreply.github.com>
2025-11-01 14:58:01 +0800

    Complete all test

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  return string.split("").reverse().join("");
 };
 
 // Do not edit below this line
--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,16 @@
-const leapYears = function() {
+const leapYears = function (year) {
+  const isDivisibleByFour = year % 4 === 0;
+  const isDivisibleByOneHundred = year % 100 === 0;
+  const isDivisibleByFourHundred = year % 400 === 0;
 
+  if (
+    isDivisibleByFour &&
+    (!isDivisibleByOneHundred || isDivisibleByFourHundred)
+  ) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
 // Do not edit below this line
