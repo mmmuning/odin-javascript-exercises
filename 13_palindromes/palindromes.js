0ce38b1b8d2d2dee1f0bc3749b98c62f862cebfd
mmmuning <217378167+mmmuning@users.noreply.github.com>
2025-11-04 13:02:02 +0800

    Complete all tests

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  const formattedStr = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
 
+  let origStr = formattedStr.split("").join("");
+  let reversedStr = formattedStr.split("").reverse().join("");
+
+  return origStr === reversedStr ? true : false;
 };
 
 // Do not edit below this line
