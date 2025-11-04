6d28a0cade6d230ddef0adc6e11d509a6e5a3136
mmmuning <217378167+mmmuning@users.noreply.github.com>
2025-11-04 15:31:47 +0800

    Complete all tests

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const getAge = function (yearOfBirth, yearOfDeath) {
+  const currentYear = new Date().getFullYear();
 
+  if (!yearOfDeath) {
+    yearOfDeath = currentYear;
+  }
+
+  return yearOfDeath - yearOfBirth;
+};
+
+const findTheOldest = function (people) {
+  return people.reduce((oldest, current) => {
+    const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+    const currentAge = getAge(current.yearOfBirth, current.yearOfDeath);
+
+    return currentAge > oldestAge ? current : oldest;
+  });
 };
 
 // Do not edit below this line
