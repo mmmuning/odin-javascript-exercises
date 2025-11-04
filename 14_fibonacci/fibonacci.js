05dd80ac5ef183927d0c86817f3c0f98b35ea49f
mmmuning <217378167+mmmuning@users.noreply.github.com>
2025-11-04 14:26:31 +0800

    Complete all tests

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+  let fibonacciSequence = [0, 1];
 
+  if (isNaN(num) || num < 0) return "OOPS";
+
+  for (let i = 2; i <= num; i++) {
+    fibonacciSequence[i] = fibonacciSequence[i - 1] + fibonacciSequence[i - 2];
+  }
+
+  return fibonacciSequence[num];
 };
 
 // Do not edit below this line
